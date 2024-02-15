3a93d6be2da903af48e0c8f29a4762f0b64dbd3d
Damien Rouzeau <86470640+DamienRouzeau@users.noreply.github.com>
2024-02-15 09:19:08 +0100

    split screen

---
+++ Assets/UI/UIBehaviour.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBehaviour : MonoBehaviour
+{
+    public PlayerSpawner playerSpawner;
+    public Camera cam;
+    public Button[] buttons;
+
+    public void TwoPlayerSelected()
+    {
+        playerSpawner.SpawnPlayer(2);
+        Destroy(cam);
+        foreach (var button in buttons)
+        {
+            Destroy(button);
+        }
+        Destroy(this);
+    }
+    public void ThreePlayerSelected()
+    {
+        playerSpawner.SpawnPlayer(3);
+        Destroy(cam);
+        foreach (var button in buttons)
+        {
+            Destroy(button);
+        }
+        Destroy(this);
+    }
+    public void FourPlayerSelected()
+    {
+        playerSpawner.SpawnPlayer(4);
+        Destroy(cam);
+        foreach (var button in buttons)
+        {
+            Destroy(button);
+        }
+        Destroy(this);
+    }
+}
