d54064fb7107d51683990362d29a46dbe9cbc777
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2024-02-15 12:53:15 +0100

    menu template

--- Assets/UI/MenuManager.cs
+++ Assets/UI/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UIBehaviour : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     public PlayerSpawner playerSpawner;
     public Camera cam;
