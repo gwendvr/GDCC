d602ebe394f59a24015e0b1cb00a22b1c9b4e4e0
voltkrugers <80946089+voltkrugers@users.noreply.github.com>
2024-03-04 12:36:45 +0100

    AddScore

---
+++ Assets/Script/Score.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public int ScorePlayer = 0;
+
+    private void Start()
+    {
+        
+    }
+}
